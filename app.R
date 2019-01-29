52e8ed8043c3679341e4b480473b9ff177ea9c43
jdeligt <joepio@gmail.com>
2019-01-29 15:15:23 +0100

    Removing 10 row limit

--- app.R
+++ app.R
@@ -4,17 +4,18 @@
 library(shiny)
 library(plotly)
 
-# Human data 
+# Human data
 library(org.Hs.eg.db)
 
 multmerge <- function(mypath){
   # Gather all RPKM files available
   filenames = list.files(path=mypath, full.names=TRUE, pattern="*_RPKM.txt")
   # Read files
-  datalist = lapply(filenames, function(x){read.table(file=x, header=T, sep='\t', row.names=1, nrows=10)})
+  #, nrows=10
+  datalist = lapply(filenames, function(x){read.table(file=x, header=T, sep='\t', row.names=1)})
   Reduce(function(x,y) {transform(merge(x,y,by=0,all=TRUE), row.names=Row.names, Row.names=NULL)}, datalist)
 }
-  
+
 rpkmdata <- data.frame(multmerge("counts/"))
 metadata <- data.frame(read.table("RNAMetaData.txt", sep='\t', header=T, row.names=1))
 
@@ -24,6 +25,7 @@ genesymbols <- NULL
 #colnames(genes) <- c("value","label")
 
 nms <- colnames(metadata)
+metadata$Time <- as.factor(metadata$Time)
 print(head(rpkmdata))
 print(head(metadata))
 
@@ -50,27 +52,27 @@ server <- function(input, output) {
 
   #add reactive data information. Dataset = built in diamonds data
   dataset <- reactive({
-    
+
     id <- NULL
     if ( ! input$gene %in% rownames(rpkmdata)) {
       id <- which(genes$SYMBOL==input$gene)
     } else {
       id <- input$gene
     }
-    
+
     dat <- data.frame(merge(t(rpkmdata[id,]), metadata, by=0))
     rownames(dat) <- dat$Row.names
     colnames(dat)[1] <- "Sample"
     colnames(dat)[2] <- "RPKM"
-    
+
     return(dat)
-    
+
   })
 
   output$boxPlot <- renderPlotly({
 
     # build graph with ggplot syntax
-    p <- ggplot(dataset(), aes_string(x=input$x, y="RPKM", color=input$color)) + 
+    p <- ggplot(dataset(), aes_string(x=input$x, y="RPKM", color=input$color)) +
        geom_boxplot(size=.75) + geom_point() + ggtitle(input$gene)
 
     # if at least one facet column/row is specified, add it
@@ -78,7 +80,7 @@ server <- function(input, output) {
     if (facets != '. ~ .') p <- p + facet_grid(facets)
 
     # display with plotly
-    ggplotly(p) %>% 
+    ggplotly(p) %>%
       layout(height=600, autosize=T, width=800)
 
   })
